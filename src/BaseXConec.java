7bcb3303b388ab6beb7922a584311deb982d9b37
TahaBakk <tahabakk2@gmail.com>
2017-03-27 17:49:46 +0200

    Initial commit

---
+++ src/BaseXConec.java
@@ -0,0 +1,88 @@
+import org.basex.api.client.ClientQuery;
+import org.basex.api.client.ClientSession;
+import org.basex.core.BaseXException;
+import org.basex.core.Context;
+import org.basex.core.cmd.CreateDB;
+import org.basex.core.cmd.XQuery;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ * Created by x3727349s on 27/03/17.
+ */
+public class BaseXConec {
+
+
+    public static void consulta(String username, String pass, String ruta, String bbdd, String consulta, String ip, String tipo){
+
+        try (ClientSession session = new ClientSession(ip, 1984, username, pass)) {//IP, PUERTO, USERNAME, PASSWORD
+            session.execute(new CreateDB(bbdd, ruta));//BBDD que se creara, y ruta del fitxero
+            try (ClientQuery query = session.query(consulta)) {//consulta
+                if (tipo == "consulta"){
+                System.out.println(query.execute());//imprimir resultado
+                }else if (tipo.equalsIgnoreCase("parserXML")){
+                    parserXML(query.execute(),"tercera");
+                }else if (tipo.equalsIgnoreCase("segunda")){
+                    parserXML(query.execute(),"segunda");
+                }
+            } catch (SAXException | ParserConfigurationException e) {
+                e.printStackTrace();
+            }
+            session.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void parserXML (String consulta, String tipo) throws ParserConfigurationException, IOException, SAXException {
+
+        int estocMaximo = 0;
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document dom = db.parse(new InputSource(new StringReader(consulta)));
+        Element docEle = dom.getDocumentElement();
+        NodeList nl = docEle.getChildNodes();
+        if (nl != null) {
+            int length = nl.getLength();
+            for (int i = 0; i < length; i++) {
+                if (nl.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                    Element el = (Element) nl.item(i);
+                    if (el.getNodeName().contains("PLANT")) {
+                        String common = el.getElementsByTagName("COMMON").item(0).getTextContent();
+                        String price = el.getElementsByTagName("PRICE").item(0).getTextContent();
+                        String availability = el.getElementsByTagName("AVAILABILITY").item(0).getTextContent();
+                        String nan = price.substring(1);
+                        double preu = Double.parseDouble(nan) * Double.parseDouble(availability);
+                        if(tipo.equalsIgnoreCase("tercera")) {
+                            System.out.println("De la planta " + common + " tenim " + availability + " exemplars. Cada exemplar té un preu de " + price + "per tant per la venta de tot l'stock ingresarem " + preu + " dolars");
+                        }else if (tipo.equalsIgnoreCase("segunda")){
+                            estocMaximo += Integer.parseInt(el.getElementsByTagName("AVAILABILITY").item(0).getTextContent());
+                        }
+                    }
+                }
+            }
+        }
+
+        if(tipo.equalsIgnoreCase("segunda")){
+            System.out.println("Al estoc hi ha un total de "+estocMaximo+" de plantes");
+        }
+
+    }
+
+
+
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,82 @@
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by x3727349s on 27/03/17.
+ */
+public class Main {
+
+
+    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+
+        String rutaXML = "/home/x3727349s/Música/examenBaseX/UF3-ExamenF-Plantes.xml";//cambiar por la ruta absoluta
+        String bbdd = "tahabakk";//Nombre de la bbdd
+        String username = "admin";//usuario de baseX
+        String pass = "admin";//Contrasenya de baseX
+        String consulta = "";//Consulta se añade en el switch dependiendo de la opcion seleccionada
+        String ip ="localhost";//ip del server/baseX
+
+        Scanner sc = new Scanner (System.in);
+
+        BaseXConec basexconec = new BaseXConec();
+
+        int numOpcion = 0;
+        do {
+            System.out.println("");
+            System.out.println("--------------------------------------------------------------------------");
+            System.out.println("1.Feu una connexió per a realitzar consultes al recurs. Indiqueu el nom de la planta de la que tenim més estoc. ");
+            System.out.println("2.Quantes plantes en total hi ha a l'estoc?");
+            System.out.println("3.Ens interessa saber el preu de tot l'estoc, planta per planta.");
+            System.out.println("--------------------------------------------------------------------------");
+            System.out.println("");
+
+            numOpcion = sc.nextInt();
+
+            switch (numOpcion) {
+                case 1:
+                    consulta = "//PLANT[AVAILABILITY = max(//PLANT/AVAILABILITY)]/COMMON";//introducimos la consulta a realizar
+                    System.out.println("Els països que conté son els següents");
+                    basexconec.consulta(username, pass, rutaXML, bbdd, consulta, ip, "consulta");//llamamos el metodo pasandole los datos cuando le paso lo de tipo es solo para saber si llamar al otro metodo o no
+                    break; // optional
+
+                case 2:
+                    consulta = "/";
+                    basexconec.consulta(username, pass, rutaXML, bbdd, consulta, ip, "segunda");
+                    break;
+
+                case 3:
+                    consulta = "/";
+                    System.out.println("Les dades del preu de l'estoc és de: ");
+                    basexconec.consulta(username, pass, rutaXML, bbdd, consulta, ip, "parserXML");
+
+                    break; // optional
+
+                /*case 4:
+                    consulta = "//record[country=\"Uganda\"]/people/population";
+                    System.out.println("La quantitat de població que hi ha en Uganda és: ");
+                    consulta(username, pass, rutaFacktbook, bbdd, consulta, ip);
+                    break; // optional
+
+                case 5:
+                    consulta = "//country[name=\"Peru\"]/province/city/name";
+                    System.out.println("Les ciutats de peru son: ");
+                    consulta(username, pass, rutaMondial, bbdd, consulta, ip);
+                    break; // optional*/
+
+                default:
+                    break;
+            }
+
+        }while (numOpcion!=0);//si es 0 sale del blucle y finaliza
+
+
+        System.out.println("Programa finalizado ^^");
+
+    }
+}
+
+
+
